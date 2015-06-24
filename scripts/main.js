81761ed3ae64c60bd5e4c65660008bb5bc9038e5
Jamie Wright <jamie@brilliantfantastic.com>
2015-06-24 12:28:47 -0400

    Refactored interruption methods

--- scripts/main.js
+++ scripts/main.js
@@ -8,21 +8,36 @@ Repeater.prototype.clear = function() {
   this.parent.empty();
 };
 
+Repeater.prototype.interrupt = function(index, content) {
+  this.interruptionIndex = index;
+  this.interruptionContent = content;
+  return this;
+};
+
+Repeater.prototype.interruptWith = function(html) {
+  html.children("span").text(this.interruptionContent);
+};
+
 Repeater.prototype.repeat = function(repeatCount) {
-  var content = this.repeatabeContent;
-  var parent = this.parent;
+  this.clear();
+
+  var repeater = this;
   for(i = 1; i <= repeatCount; i++) {
-    var html = content.clone();
-    if (i % 5 === 0) {
-      html.children("span").text("Hello, Jon");
+    var html = repeater.repeatabeContent.clone();
+    if (repeater.shouldInterrupt(i)) {
+      repeater.interruptWith(html);
     }
-    parent.append(html[0].outerHTML);
+    repeater.parent.append(html[0].outerHTML);
   }
+};
+
+Repeater.prototype.shouldInterrupt = function(index) {
+  return index % this.interruptionIndex === 0;
 };
 
 $(function() {
   function repeatContent(repeatCount) {
-    new Repeater("[data-behavior~=repeatable-content]").repeat(repeatCount);
+    new Repeater("[data-behavior~=repeatable-content]").interrupt(5, "Hello, Jon").repeat(repeatCount);
   };
 
   repeatContent(300);
