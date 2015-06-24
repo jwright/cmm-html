b14a6fbb45d7c06f3bc11040d6bfcabef0e00dd2
Jamie Wright <jamie@brilliantfantastic.com>
2015-06-24 12:15:48 -0400

    Create prototype to perform the repeat

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,28 @@
+function Repeater(selector) {
+  this.selector = selector;
+  this.repeatabeContent = $(this.selector);
+  this.parent = this.repeatabeContent.parent();
+};
+
+Repeater.prototype.clear = function() {
+  this.parent.empty();
+};
+
+Repeater.prototype.repeat = function(repeatCount) {
+  var content = this.repeatabeContent;
+  var parent = this.parent;
+  for(i = 1; i <= repeatCount; i++) {
+    var html = content.clone();
+    if (i % 5 === 0) {
+      html.children("span").text("Hello, Jon");
+    }
+    parent.append(html[0].outerHTML);
+  }
+};
+
 $(function() {
   function repeatContent(repeatCount) {
-    var content = $("[data-behavior~=repeat-content]");
-    var parent = content.parent();
-    parent.empty();
-
-    for(i = 1; i <= repeatCount; i++) {
-      var html = content.clone();
-      if (i % 5 === 0) {
-        html.children("span").text("Hello, Jon");
-      }
-      parent.append(html[0].outerHTML);
-    }
+    new Repeater("[data-behavior~=repeatable-content]").repeat(repeatCount);
   };
 
   repeatContent(300);
